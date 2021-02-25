1321fa206d7eb3b5cdbe7547571e078c80f6bc58
Katherine Lim <k4lim@ucsd.edu>
2021-02-24 20:46:45 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'R2WgtOrDFXPsKoaBbyVIYmBxTnLqtu7j';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.88, -117.246],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.88073802809082, -117.23627204348655]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,13 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+  var rsvpEmail= request.body.rsvpEmail;
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail); // add to current data
+
+  // let us send back some data so the brower knows it worked
+  response.send(rsvpEmail);
+}
